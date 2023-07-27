853d9155f9c13ec68eb803a48e72185cdef62844
Boyu Gao <44675030+EZ4BYG@users.noreply.github.com>
2023-07-27 13:56:49 +0200

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -8,5 +8,8 @@ print(b)
 ## New branch
 print('Code from the boygao branch')
 
-## New edition from Github
-print('New contnets added by others')
+## New edition 1 from Github
+print('New contnets added from Github')
+
+## New edition 2 from person 1
+print('New contents added from person 1')
